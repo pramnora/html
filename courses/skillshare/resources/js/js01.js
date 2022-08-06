d024408bf6f83e0ae92a00ad1aa07aa6b7044f38
Paul Ramnora <pramnora@yahoo.com>
2022-08-06 10:36:17 +0100

    Added Javascript footnote: Powered by
    Added Javascript footnote to all pages: Powered by...

--- courses/skillshare/resources/js/js01.js
+++ courses/skillshare/resources/js/js01.js
@@ -1,3 +1,16 @@
 function writeWebSiteMainH1TitleText(){
- document.write("<h1>Paul Ramnora [*<span class='clsWebSiteMainH1TitleText'>Web Site</span>*]</h1>");
+ document.write("<h1>" +
+                "Paul Ramnora " +
+                "[*" + 
+                "<span class='clsWebSiteMainH1TitleText'>" + 
+                "Web Site" + 
+                "</span>" + 
+                "*]" +
+                "</h1>");
+}
+
+function writeThisWebSiteIsPoweredByMessage(){
+ document.write("This web page is <b><i>*powered*</i></b> by the following technologies:"+
+                "<br />" + 
+                "Desktop PC/Windows 10 Professional/Notepad: HTML/CSS/Javascript");
 }
